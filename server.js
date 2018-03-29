320b25d9b9907f0d82239495ac2256c480ea244b
caue <nncl@live.com>
2018-03-29 18:23:58 -0300

    Add server

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require('express')
+    , app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
